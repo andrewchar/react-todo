499735084858f8a7ee8b077d2e1bb6e69665b13f
Andrew Charlebois <andrew.charlebois@caseware.com>
2018-08-09 10:35:42 -0400

    change remove_todo component from class based to functional component

--- src/components/remove_todo.js
+++ src/components/remove_todo.js
@@ -1,19 +1,12 @@
-import React, { Component } from 'react';
+import React from 'react';
 
-class RemoveTodoItem extends Component {
-
-    // removeTodoItem(i) {
-    //     console.log(i);
-    // }
-
-    render() {
-        return (
-            <span 
-                style={{cursor: 'pointer', border: '1px solid red', margin: '5px'}}
-                onClick={() => this.props.removeTodoItem(this.props.index)}>
-            &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;x</span>
-        )
-    }
+const RemoveTodoItem = (props) => {
+    return (
+        <span 
+            style={{cursor: 'pointer', border: '1px solid red', margin: '5px'}}
+            onClick={() => props.removeTodoItem(props.index)}>
+        &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;x</span>
+    )
 }
 
 export default RemoveTodoItem;
